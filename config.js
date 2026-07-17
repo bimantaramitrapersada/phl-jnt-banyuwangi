176ad70a5269f5d8623c12a3e28a97be80077cb3
bimantaramitrapersada <bimantaramitrapersada@gmail.com>
2026-07-17 16:17:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxtdZWR1WKkX8n0CZ-0HT1vldJpI5KinpSXSf-RX4Z2qfd0QU6F2j8AVQJvgAgoN36G/exec',
+  SECRET_TOKEN: 'bimantaramitrapersadabanyuwangi',
+ KANTOR: { 
+  lat: -8.285912968376206,
+  lng: 114.31550627803054
+},
+  RADIUS_MAX: 100
+};
